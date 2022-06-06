64a558b17c81dba5c93cfe65fca629dbb67dd545
genelem <genesiselealmunoz@gmail.com>
2022-06-06 16:52:08 -0300

    arreglos

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -5,10 +5,6 @@ const { Op } = require("sequelize");
 const { validationResult } = require("express-validator");
 const { promiseImpl } = require('ejs');
 
-
-
-
-
 const mainController = {
   home: (req, res) => {
     db.Book.findAll({
@@ -28,7 +24,7 @@ const mainController = {
   bookSearch: (req, res) => {
     res.render('search');
 
-   },
+  },
   bookSearchResult: function (req, res) {                      //busqueda de libro
     let bookFind = req.body.book
     db.Book.findAll({
@@ -37,18 +33,18 @@ const mainController = {
       }
     })
       .then(function (Book) {
-        res.send('books',{Book:Book})
+        res.send('books', { Book: Book })
       })
   },
-  deleteBook:  (req, res) => {                            // eliminar libro
+  deleteBook: (req, res) => {                            // eliminar libro
     db.Book.destroy({
-        where: {
-            id: req.params.id
-        }
+      where: {
+        id: req.params.id
+      }
     })
-        .then(() => {
-            res.redirect('books/:id')
-        })
+      .then(() => {
+        res.redirect('books/:id')
+      })
   },
   authors: (req, res) => {
     db.Author.findAll()
@@ -57,13 +53,13 @@ const mainController = {
       })
       .catch((error) => console.log(error));
   },
-  authorBooks:(req, res) => {
+  authorBooks: (req, res) => {
     db.Book.findByPk(req.params.id)
       .then(book => {
         res.render('bookDetail', { book });
       });
-    
-    },
+
+  },
   register: (req, res) => {
     res.render('register');
   },
@@ -83,49 +79,52 @@ const mainController = {
   login: (req, res) => {
     // Implement login process
     res.render('login');
-  
+
   },
-  processLogin:  (req, res, next) => {
+  processLogin: (req, res, next) => {
     const errores = validationResult(req);
 
-    if(!errores.isEmpty()){
-        return res.render("login", { 
-            errores: errores.errors,
-            old: req.body 
-        });
+    if (!errores.isEmpty()) {
+      return res.render("login", {
+        errores: errores.errors,
+        old: req.body
+      });
     }
     db.User.findOne({
-        where: {
-            email: req.body.email,
-            pass:req.body.pass = bcryptjs.hashSync(req.body.password, 8)
-        }
-    }).then( usuarioEncontrado => {
-        req.session.usuarioLogueado = usuarioEncontrado;
-        if(req.body.recordame){
-            res.cookie("recordame", usuarioEncontrado, { maxAge: 60000 * 60 * 24 })
-        }
-        return res.redirect("/");
-    })  
+      where: {
+        email: req.body.email,
+        pass: req.body.pass = bcryptjs.hashSync(req.body.password, 8)
+      }
+    }).then(usuarioEncontrado => {
+      req.session.usuarioLogueado = usuarioEncontrado;
+      if (req.body.recordame) {
+        res.cookie("recordame", usuarioEncontrado, { maxAge: 60000 * 60 * 24 })
+      }
+      return res.redirect("/");
+    })
 
-},
+  },
+  editBook: function (req, res) {
+    db.Book.findByPk(req.params.id)
+      .then(book => {
+        res.render("editBook", { id: req.params.id })
+      })
 
-editBook: function(req, res){                        
-      db.Book.findByPk(req.params.id)
-          .then(book => {
-              res.render("editBook", { id:req.params.id })
-          })
-    
   },
   processEdit: (req, res) => {
     db.Book.findByPk(req.params.id)
-          .then(book => {
-    res.render('home');
-   })
-  
+      .then(book => {
+        res.render('home');
+      })
+
   }
 
-   }
-    
- 
+}
+
+
+
+module.exports = mainController;
+
+
 
 module.exports = mainController;
